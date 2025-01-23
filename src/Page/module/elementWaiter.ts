5c4a581e216c139c94a44fd27380016ba3318178
Yiero <aluibyiero@qq.com>
2025-01-23 22:44:13 +0800

    fix(0.1.11): - 修复 `elementWaiter` 无法正常使用的问题.
    Took 20 minutes

--- src/Page/module/elementWaiter.ts
+++ src/Page/module/elementWaiter.ts
@@ -25,6 +25,117 @@ export interface IElementWaiterOption {
 }
 
 /**
+ * 兑现
+ */
+const returnElement = <T>(
+	selector: string,
+	options: IElementWaiterOption,
+	resolve: ( value: ( T | PromiseLike<T> ) ) => void,
+	reject: ( reason?: any ) => void,
+) => {
+	// 延时触发
+	setTimeout( () => {
+		const element = options.parent.querySelector<HTMLElement>( selector );
+		// 空元素, 抛出异常
+		if ( !element ) {
+			reject( new Error( 'Void Element' ) );
+			return;
+		}
+		
+		resolve( element as T );
+	}, options.delayPerSecond * 1000 );
+};
+
+
+/**
+ * 通过计时器获取元素
+ */
+const getElementByTimer = <T>(
+	selector: string,
+	options: IElementWaiterOption,
+	resolve: ( value: ( T | PromiseLike<T> ) ) => void,
+	reject: ( reason?: any ) => void,
+) => {
+	const intervalDelay = 100;          // 间隔时间
+	let intervalCounter = 0;    // 计数器
+	const maxIntervalCounter = Math.ceil( ( ( options.timeoutPerSecond ) * 1000 ) / intervalDelay );
+	
+	const timer = window.setInterval( () => {
+		// 定时器计数
+		if ( ++intervalCounter > maxIntervalCounter ) {
+			// 超时清除计数器
+			clearInterval( timer );
+			// reject访问
+			returnElement( selector, options, resolve, reject );
+			return;
+		}
+		
+		// 尝试获取元素
+		const element = options.parent.querySelector<HTMLElement>( selector );
+		
+		// 获取到元素时
+		if ( element ) {
+			clearInterval( timer );
+			returnElement( selector, options, resolve, reject );
+		}
+	}, intervalDelay );
+};
+
+
+/**
+ * 通过元素监听器获取元素
+ */
+const getElementByMutationObserver = <T>(
+	selector: string,
+	options: IElementWaiterOption,
+	resolve: ( value: ( T | PromiseLike<T> ) ) => void,
+	reject: ( reason?: any ) => void,
+) => {
+	// 声明定时器
+	const timer: number = options.timeoutPerSecond && window.setTimeout( () => {
+		// 关闭监听器
+		observer.disconnect();
+		
+		// 返回元素 reject
+		returnElement( selector, options, resolve, reject );
+	}, ( options.timeoutPerSecond ) * 1000 );
+	
+	const observeElementCallback = ( mutations: MutationRecord[] ) => {
+		mutations.forEach( ( mutation ) => {
+			mutation.addedNodes.forEach( ( addNode ) => {
+				if ( addNode.nodeType !== Node.ELEMENT_NODE ) {
+					return;
+				}
+				
+				// 获取元素
+				const addedElement = addNode as HTMLElement;
+				const element = addedElement.matches( selector )
+					? addedElement
+					: addedElement.querySelector<HTMLElement>( selector );
+				
+				// 如果获取到元素
+				if ( element ) {
+					// 清空定时器
+					timer && clearTimeout( timer );
+					
+					// 返回元素
+					returnElement( selector, options, resolve, reject );
+				}
+			} );
+		} );
+	};
+	
+	// 声明监听器
+	const observer = new MutationObserver( observeElementCallback );
+	
+	observer.observe( options.parent, {
+		subtree: true,
+		childList: true,
+	} );
+	return true;
+};
+
+/**
  * 等待元素载入
  *
  * @param selector CSS选择器
@@ -34,124 +145,31 @@ export interface IElementWaiterOption {
  */
 export function elementWaiter<T extends HTMLElement>(
 	selector: string,
-	options: Partial<IElementWaiterOption> = {},
+	options?: Partial<IElementWaiterOption>,
 ): Promise<T> {
-	// 默认值赋予
-	const {
-		parent = document,
-		timeoutPerSecond = 20,
-		delayPerSecond = 0.5,
-	} = options;
+	const elementWaiterOptions: IElementWaiterOption = {
+		parent: document,
+		
+		timeoutPerSecond: 20,
+		delayPerSecond: 0.5,
+		...options,
+	};
 	
 	return new Promise( ( resolve, reject ) => {
-		/**
-		 * 工具函数: 延时返回目标元素函数
-		 * 提供两种获取目标元素的方式:
-		 * 1. 通过 Promise 返回值获取元素
-		 * 2. 根据分发的事件 'ElementUpdate', 获取回调, 回调参数 e.detail 为目标元素
-		 * */
-		const returnElement = ( selector: string ) => {
-			// 延时触发
-			setTimeout( () => {
-				const element = parent.querySelector<HTMLElement>( selector );
-				// 空元素, 抛出异常
-				if ( !element ) {
-					reject( new Error( 'Void Element' ) );
-					return;
-				}
-				
-				// 触发事件 (element = e.detail);
-				// window.dispatchEvent( new CustomEvent( 'ElementUpdate', { detail: element } ) );
-				
-				// 返回元素
-				resolve( element as T );
-			}, delayPerSecond * 1000 );
-		};
-		
 		// 分支1 - 元素已经载入, 直接获取到元素
-		const getElement = ( selector: string ): boolean => {
-			const element = parent.querySelector<HTMLElement>( selector );
-			if ( element ) {
-				returnElement( selector );
-				return true;
-			}
-			return false;
-		};
-		const isContinue = getElement( selector );
-		if ( !isContinue ) return;
+		const targetElement = elementWaiterOptions.parent.querySelector<HTMLElement>( selector );
+		if ( targetElement ) {
+			returnElement( selector, elementWaiterOptions, resolve, reject );
+			return;
+		}
 		
 		// 分支2 - 元素未载入, 使用MutationObserver获取元素
-		const mutationGetElement = ( selector: string ): boolean => {
-			// 声明定时器
-			const timer: number = timeoutPerSecond && window.setTimeout( () => {
-				// 关闭监听器
-				observer.disconnect();
-				
-				// 返回元素 reject
-				returnElement( selector );
-			}, ( timeoutPerSecond ) * 1000 );
-			
-			const observeElementCallback = ( mutations: MutationRecord[] ) => {
-				mutations.forEach( ( mutation ) => {
-					mutation.addedNodes.forEach( ( addNode ) => {
-						if ( addNode.nodeType !== Node.ELEMENT_NODE ) {
-							return;
-						}
-						
-						// 获取元素
-						const addedElement = addNode as HTMLElement;
-						const element = addedElement.matches( selector )
-							? addedElement
-							: addedElement.querySelector<HTMLElement>( selector );
-						
-						// 如果获取到元素
-						if ( element ) {
-							// 清空定时器
-							timer && clearTimeout( timer );
-							
-							// 返回元素
-							returnElement( selector );
-						}
-					} );
-				} );
-			};
-			
-			// 声明监听器
-			const observer = new MutationObserver( observeElementCallback );
-			
-			observer.observe( <HTMLElement> parent, {
-				subtree: true,
-				childList: true,
-			} );
-			return true;
-		};
 		if ( MutationObserver ) {
-			mutationGetElement( selector );
+			getElementByMutationObserver( selector, elementWaiterOptions, resolve, reject );
+			return;
 		}
 		
 		// 分支3 - 元素未载入, 浏览器无MutationObserver类, 使用定时器获取元素
-		const intervalDelay = 100;  // 间隔时间
-		let intervalCounter = 0;    // 计数器
-		const maxIntervalCounter = Math.ceil( ( ( timeoutPerSecond ) * 1000 ) / intervalDelay );
-		
-		const timer = window.setInterval( () => {
-			// 定时器计数
-			if ( ++intervalCounter > maxIntervalCounter ) {
-				// 超时清除计数器
-				clearInterval( timer );
-				// reject访问
-				returnElement( selector );
-				return;
-			}
-			
-			// 尝试获取元素
-			const element = ( <HTMLElement> parent ).querySelector( selector ) as HTMLElement;
-			
-			// 获取到元素时
-			if ( element ) {
-				clearInterval( timer );
-				returnElement( selector );
-			}
-		}, intervalDelay );
+		getElementByTimer( selector, elementWaiterOptions, resolve, reject );
 	} );
 }
